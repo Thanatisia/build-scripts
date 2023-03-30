48cc233a64f0bc65b8804b09c29fc466cff50d54
Thanatisia <55834101+Thanatisia@users.noreply.github.com>
2023-03-30 14:40:55 +0800

    Update 2023-03-30 1431H | refactor-and-new-build Pretty massive update and reorganization of template structure
    [New]
    - Created a automated downloader script to download all 7 parts automatically
    - Added new build script repository for the Command Line/terminal Fuzzy Finder utility - 'junegunn/fzf'
    
    [Updates]
    - Updated README.md with new info on the changes
    - Updated CONTRIBUTING.md with new info on how to use the new template, as well as explanations
    - Updated the template files in 'docs/contributions/templates/shellscript/[package-manager]' to reflect the new structure and syntaxes
    
    [Testing]
    - Updated the current template of a combined 'compile.sh' to split into 7 components (7 script files)
        1. settings.sh
        2. setup.sh
        3. compile.sh
        4. installer.sh
        5. clean.sh
        6. uninstaller.sh
        7. start.sh
        - Currently testing using junegunn/fzf as a starting point, initial tests proved to be modifiable/customizable/modular, portable, reusable

---
+++ docs/contributions/templates/shellscript/general/setup.sh
@@ -0,0 +1,47 @@
+#!/bin/env bash
+: "
+Setup repository and system structure for compilation
+
+Method: Compile from Scratch to ensure that it is on the latest version
+
+Target Base System: Debian
+Target Package Manager: apt
+"
+
+source_files()
+{
+    # Source settings file
+    source $PWD/settings.sh
+}
+
+install_dependencies()
+{
+    : "
+    Install and setup dependencies
+    "
+    # Install dependencies
+    # sudo [your-package-installer-here] "${DEPENDENCIES[@]}"
+}
+
+obtain_repository()
+{
+    # Clone git repository
+    git clone --depth 1 "$SRC_URL"
+}
+
+setup()
+{
+    : "
+    Setup all dependencies required to build
+    "
+    source_files
+    install_dependencies
+    obtain_repository
+
+    # Change directory into source code
+    cd $PKG_NAME
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    setup
+fi
