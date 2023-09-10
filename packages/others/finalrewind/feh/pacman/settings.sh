e9a802a0ee447ba82142db8e81c4eb4a65d2c70b
Thanatisia <55834101+Thanatisia@users.noreply.github.com>
2023-09-10 23:20:22 +0800

    Update 2023-09-10 2319H | updated-package-build [Updates] - Updated package list - Added pacman build for package 'finalrewind/feh'

---
+++ packages/others/finalrewind/feh/pacman/settings.sh
@@ -0,0 +1,15 @@
+# Initialize Variables
+
+## Mutable Variables
+REPO_AUTHOR="finalrewind"
+REPO_PROJECT="feh"
+REPO_DOMAIN="https://git.finalrewind.org"
+DEPENDENCIES=(git base-devel libcurl4-openssl-dev libx11-dev libxt-dev libimlib2-dev libxinerama-dev libjpeg-progs) # Set Dependencies
+EDITOR="vim" # Set Default Editor
+
+## Derivatives
+REPO_URL=$REPO_DOMAIN/${REPO_PROJECT}
+
+## Build System
+CC="make"
+CFLAGS=""
